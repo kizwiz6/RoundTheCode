f70eb93c3f3d7c48245050d6da5c91ce391694e5
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-04 23:47:54 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundTheCode
+{
+    public abstract class Vehicle
+    {
+        public int Wheels { get; set; }
+    }
+
+    public class Car : Vehicle
+    {
+    }
+
+    public class Motorbike : Vehicle
+    {
+    }
+
+    public class Wheel
+    {
+        public int CountTotalWheels(Vehicle[] vehicles)
+        {
+            var wheelCount = 0;
+
+            foreach (var vehicle in vehicles)
+            {
+                wheelCount+= vehicle.Wheels;
+            }
+
+            return wheelCount;
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundTheCode
+{
+    internal class Team
+    {
+    }
+    public class TeamService
+    {
+        public void Create(Team team) { }
+        public void Update(int id, Team team){}
+        public void Delete(int id) { }
+    }
+
+    public class  CompetitionService
+    {
+        public void AddToCompetition(int teamId, competitionId) { }
+
+    }
+}
